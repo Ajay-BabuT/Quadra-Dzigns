b406b6f8de64c4a5a25e0f1a74d29a22e4650bb2
Ajay Babu T <ajaybabu9895@gmail.com>
2025-01-17 15:44:39 +0530

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+/*-------------------nav--------------------------*/
+const body = document.querySelector("body"),
+      nav = document.querySelector("nav"),
+      sidebarOpen = document.querySelector(".sidebarOpen"),
+      siderbarClose = document.querySelector(".siderbarClose");
+
+let getMode = localStorage.getItem("mode");
+
+// JS code to toggle sidebar
+sidebarOpen.addEventListener("click", () => {
+  nav.classList.add("active");
+});
+
+body.addEventListener("click", (e) => {
+  let clickedElm = e.target;
+  if (!clickedElm.classList.contains("sidebarOpen") && !clickedElm.classList.contains("menu")) {
+    nav.classList.remove("active");
+  }
+});
+/*-------------------nav--------------------------*/
+
+document.querySelectorAll('.vdo-cards video').forEach(video => {
+  // Play video when mouse enters
+  video.addEventListener('mouseenter', () => {
+    video.play();
+  });
+
+  // Pause video when mouse leaves
+  video.addEventListener('mouseleave', () => {
+    video.pause();
+    video.currentTime = 0; // Reset to the beginning
+  });
+});
+
+
+
+// service cards 
+document.querySelectorAll('.vdo-cards video').forEach(video => {
+  video.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+  });
+});
+
+
+
+
+//form script
+
+$("#submit-form").submit((e)=>{
+            e.preventDefault()
+            $.ajax({
+                url:"https://script.google.com/macros/s/AKfycbw07wFYvy8KKu9fFjqFsEsn8lFK0MZUB8bnJhbXm02GNcGJrZtZ43r6DjrNTqjLako8/exec",
+                data:$("#submit-form").serialize(),
+                method:"post",
+                success:function (response){
+                    alert("Form submitted successfully")
+                    window.location.reload()
+                    //window.location.href="https://google.com"
+                },
+                error:function (err){
+                    alert("Something Error")
+    
+                }
+            })
+        })
